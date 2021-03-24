2dbbe1e3441737fe610744410b5f57594eca1a33
cococrunchz <apelino40@gmail.com>
2021-03-25 00:16:52 +0800

    edited

--- app/src/main/java/com/example/myapplication/rangerScreen.java
+++ app/src/main/java/com/example/myapplication/rangerScreen.java
@@ -14,59 +14,65 @@ import org.w3c.dom.Text;
 
 public class rangerScreen extends AppCompatActivity implements View.OnClickListener {
 
+    ImageView archer, bscArcher, back, next, lore, calc;
+    TextView Class, hp, mp, physAtk, physDef, mgAtk, mgDef, str, agi, intel;
+    EditText inputLvl;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranger_screen);
 
-        ImageView archer = findViewById(R.id.archer);
-        ImageView bscArcher = findViewById(R.id.basic_archer);
-        ImageButton back = findViewById(R.id.backBtn);
-        ImageButton next = findViewById(R.id.nextBtn);
-        ImageButton lore = findViewById(R.id.loreBtn);
-        ImageButton calc = findViewById(R.id.calc);
+        archer.findViewById(R.id.archer);
+        bscArcher.findViewById(R.id.basic_archer);
+        back.findViewById(R.id.backBtn);
+        next.findViewById(R.id.nextBtn);
+        lore.findViewById(R.id.loreBtn);
+        calc.findViewById(R.id.calc);
+        Class.findViewById(R.id.rangerClass);
+        Class.setText("Basic Archer");
 
         calc.setOnClickListener(this);
-
 
         archer.setVisibility(View.INVISIBLE);
         bscArcher.setVisibility(View.VISIBLE);
 
     }
+
     @Override
-    public void onClick(View view){
+    public void onClick(View view) {
 
-        TextView Class = findViewById(R.id.rangerClass);
-        EditText inputLvl = findViewById(R.id.inputLvl);
-        TextView hp = findViewById(R.id.Health);
-        TextView mp = findViewById(R.id.Mana);
-        TextView physAtk = findViewById(R.id.PhysAtk);
-        TextView physDef = findViewById(R.id.PhysDef);
-        TextView mgAtk = findViewById(R.id.MgAtk);
-        TextView mgDef = findViewById(R.id.MgDef);
-        TextView str = findViewById(R.id.Str);
-        TextView agi = findViewById(R.id.Agi);
-        TextView intel = findViewById(R.id.Int);
+        inputLvl.findViewById(R.id.inputLvl);
+        hp.findViewById(R.id.Health);
+        mp.findViewById(R.id.Mana);
+        physAtk.findViewById(R.id.PhysAtk);
+        physDef.findViewById(R.id.PhysDef);
+        mgAtk.findViewById(R.id.MgAtk);
+        mgDef.findViewById(R.id.MgDef);
+        str.findViewById(R.id.Str);
+        agi.findViewById(R.id.Agi);
+        intel.findViewById(R.id.Int);
 
-
-        hero basicArcher = new hero("Mei","Ranger",1000,0 ,9 ,50,20, 30);
+        hero basicArcher = new hero("Mei", "Ranger", 1000, 1, 9, 50, 20, 30);
 
         //basicArcher.setHerolvl(0); nothing changes when included, app also crashes when the rest of the stats are included
 
         double level = basicArcher.getHerolvl();
 
-        //level = Double.parseDouble(inputLvl.getText().toString()); crashes when included
 
-        if (level <= 20){ //undecided
+        basicArcher.getHerolvl();
 
+        if (basicArcher.getHerolvl() <= 20) { //undecided
+            level = Double.parseDouble(inputLvl.getText().toString()); //crashes when included
             hp.setText(String.valueOf(Math.round(basicArcher.hpGrowth()))); //output won't show up
             mp.setText(String.valueOf(Math.round(basicArcher.mpGrowth()))); //''
-            physAtk.setText(String.valueOf(Math.round(basicArcher.physAtkGrowth()))); //shows up but only shows the initial base stat
+            physAtk.setText(String.valueOf(Math.round(basicArcher.physAtkGrowth())));//shows up but only shows the initial base stat
+            physDef.setText(String.valueOf(Math.round(basicArcher.physDefGrowth())));
 
         }
 
 
     }
 
-
 }
+
